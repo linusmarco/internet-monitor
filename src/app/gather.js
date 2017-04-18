26f8372ae7cffd7bc18c37f69bd51caa75fb0668
Linus Marco <lmarco163@gmail.com>
2017-04-18 19:10:34 -0400

    record time before test, return null value on error

--- src/app/gather.js
+++ src/app/gather.js
@@ -1,15 +1,24 @@
 const speedTest = require('speedtest-net');
 
-let getSpeeds = function(callback) {
+const getSpeeds = function(callback) {
+    const now = new Date();
+    
     speedTest({maxTime: 5000})
         .on('data', data => {
             console.log("Retrieved speed data");
-            data["time"] = new Date();
+            data["time"] = now;
             callback(data);
         })
         .on('error', err => {
             console.error(err);
-            return null;
+            callback({
+                "time": now,
+                "speeds": {
+                    "download": NaN,
+                    "upload": NaN
+                },
+                "error": err.toString()
+            });
         });
 };
 
